893eac6d4985bc5e9580b11d3121dfef4c64c6da
KC Giraldo-Barbosa <kcgbarbosa@gmail.com>
2026-07-24 18:00:31 -0400

    scroll to top on route change

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import ScrollToTop from '../components/ScrollToTop';
 import { Outlet } from 'react-router-dom';
 import { AppStatusContext } from '../store/contexts';
 import { CircleLoader } from 'react-spinners';
@@ -11,6 +12,7 @@ function Layout() {
 
   return (
     <div className="flex flex-col min-h-screen ">
+      <ScrollToTop />
       <Navbar />
       {isLoading ? (
         <div className="flex flex-1 flex-col items-center justify-center gap-4">
